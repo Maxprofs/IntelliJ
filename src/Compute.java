b86447c233b7219a1f18aca4b3cdcad0ab6658fa
Matt <pczerokey@gmail.com>
2017-03-06 17:52:24 -0500

    Project In The Works
    noob stuff

---
+++ src/Compute.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 3/6/17.
+ */
+public class Compute
+{
+    double num1,num2;
+    public double Compute(int x, int y)
+    {
+        num1 = x;
+        num2 = y;
+        double calculation;
+        calculation = num1 * num2;
+        return calculation;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double num1, num2;
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Give me the first # you want to multiply");
+        num1 = keyboard.nextDouble;
+        System.out.println("Give me the second # you want to multiply");
+        num2 = keyboard.nextDouble;
+        Compute multiplation = new Compute(num1,num2);
+        System.out.println(calculation);
+    }
+}
